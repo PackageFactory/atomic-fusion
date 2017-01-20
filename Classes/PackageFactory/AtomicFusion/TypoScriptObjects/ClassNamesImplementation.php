8dff4e28a3a780c243f768a0bcfa620ea4b74955
Martin Ficzel <ficzel@sitegeist.de>
2017-01-20 11:43:39 +0100

    TASK: Apply migration Neos.Fusion-20161125013710
    Adjusts code to package renaming from "TYPO3.TypoScript" to
    "Neos.Fusion"

--- Classes/PackageFactory/AtomicFusion/TypoScriptObjects/ClassNamesImplementation.php
+++ Classes/PackageFactory/AtomicFusion/TypoScriptObjects/ClassNamesImplementation.php
@@ -14,7 +14,7 @@ namespace PackageFactory\AtomicFusion\TypoScriptObjects;
  */
 
 use Neos\Flow\Annotations as Flow;
-use TYPO3\TypoScript\TypoScriptObjects\ArrayImplementation;
+use Neos\Fusion\TypoScriptObjects\ArrayImplementation;
 
 /**
  * A TypoScript ClassNames-Object
